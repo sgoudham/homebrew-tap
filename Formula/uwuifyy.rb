944a2d804a3812008a81a818a94689edc0450ea5
Hamothy <58985301+sgoudham@users.noreply.github.com>
2022-01-31 12:15:11 +0000

    Add uwuifyy formula

---
+++ Formula/uwuifyy.rb
@@ -0,0 +1,12 @@
+class UwUifyy < Formula
+  desc "A robust, customizable, efficient and easy-to-use command line application to uwu'ify your text!"
+  homepage "https://github.com/sgoudham/uwuifyy"
+  url "https://github.com/sgoudham/uwuifyy/releases/download/v0.1.1/uwuifyy-x86_64-apple-darwin.tar.gz"
+  sha256 "74097dee8536f6103f4702937d88fc518b5deafcf4fb43d0489f9058e0c1dd43"
+  version "0.1.1"
+  license "MIT"
+
+  def install
+    bin.install "uwuifyy"
+  end
+end
