2affd9653aacac851bb8c470e6f6edf08e6d6cbb
sgoudham <sgoudham@gmail.com>
2023-07-04 23:49:27 +0100

    fix(git-view): install path

--- Formula/git-view.rb
+++ Formula/git-view.rb
@@ -1,11 +1,11 @@
 class GitView < Formula
-  desc "A git sub-command to view your git repository in the web browser"
+  desc "A git sub-command to view your git repository on GitHub"
   homepage "https://github.com/sgoudham/git-view"
   url "https://github.com/sgoudham/git-view/releases/download/v0.1.0/git-view-x86_64-apple-darwin.tar.gz"
   sha256 "34a1cdeadf11d1ce2ce88ab7e1fad2d4acede39720a4f94a5d1ccb4016e11c9b"
   license "MIT"
 
   def install
-    bin.install "git-view"
+    bin.install "git-view-x86_64-apple-darwin/git-view"
   end
 end
