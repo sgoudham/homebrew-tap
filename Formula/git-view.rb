c6d5848a2e1f794067760bfa2dd70a7615795385
Hamothy <58985301+sgoudham@users.noreply.github.com>
2023-07-05 00:33:51 +0100

    git-view -> 1.0.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/git-view.rb
+++ Formula/git-view.rb
@@ -1,8 +1,8 @@
 class GitView < Formula
   desc "A git sub-command to view your git repository on GitHub"
   homepage "https://github.com/sgoudham/git-view"
-  url "https://github.com/sgoudham/git-view/releases/download/v0.1.0/git-view-x86_64-apple-darwin.tar.gz"
-  sha256 "34a1cdeadf11d1ce2ce88ab7e1fad2d4acede39720a4f94a5d1ccb4016e11c9b"
+  url "https://github.com/sgoudham/git-view/releases/download/v1.0.0/git-view-x86_64-apple-darwin.tar.gz"
+  sha256 "fdd50f2beabb81ef8765a1a2cad2337a0c870ac65d349162aa30ab890a3bb060"
   license "MIT"
 
   def install
